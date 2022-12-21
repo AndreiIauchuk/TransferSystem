b3678a8a276f2ffd01add9e6de7e42b75941a43d
AndreiIauchuk <iovchukandrew@gmail.com>
2022-12-21 17:10:31 +0100

    init commit

---
+++ src/main/java/com/nordea/iovchuk/transfer_system/TransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nordea.iovchuk.transfer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nordea/iovchuk/transfer_system/TransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordea.iovchuk.transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
